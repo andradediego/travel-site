b323386eaa22591c2f38ca94da8ddaa58f1a0d94
andradediego <andrade.diego@outlook.com>
2018-01-21 17:59:24 -0200

    finished animations on scrolling

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -10441,6 +10441,10 @@ return jQuery;
 "use strict";
 
 
+var _jquery = __webpack_require__(0);
+
+var _jquery2 = _interopRequireDefault(_jquery);
+
 var _MobileMenu = __webpack_require__(2);
 
 var _MobileMenu2 = _interopRequireDefault(_MobileMenu);
@@ -10452,7 +10456,8 @@ var _RevealOnScroll2 = _interopRequireDefault(_RevealOnScroll);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var mobileMenu = new _MobileMenu2.default();
-var revealOnScroll = new _RevealOnScroll2.default();
+new _RevealOnScroll2.default((0, _jquery2.default)('.feature-item'), "85%");
+new _RevealOnScroll2.default((0, _jquery2.default)('.testimonial'), "75%");
 
 /***/ }),
 /* 2 */
@@ -10530,10 +10535,11 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var RevealOnScroll = function () {
-  function RevealOnScroll() {
+  function RevealOnScroll(els, offset) {
     _classCallCheck(this, RevealOnScroll);
 
-    this.itemsToReveal = (0, _jquery2.default)('.feature-item');
+    this.itemsToReveal = els;
+    this.offsetPercentege = offset;
     this.hideInitially();
     this.createWaypoints();
   }
@@ -10546,6 +10552,7 @@ var RevealOnScroll = function () {
   }, {
     key: 'createWaypoints',
     value: function createWaypoints() {
+      var that = this;
       this.itemsToReveal.each(function () {
         var currentItem = this;
         new Waypoint({
@@ -10553,7 +10560,7 @@ var RevealOnScroll = function () {
           handler: function handler() {
             (0, _jquery2.default)(currentItem).addClass('reveal-item--is-visible');
           },
-          offset: "85%"
+          offset: that.offsetPercentege
         });
       });
     }
