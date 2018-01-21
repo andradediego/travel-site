e66d90ed7f8b00c0e97faa544e239a55c25ba208
andradediego <andrade.diego@outlook.com>
2018-01-21 14:12:07 -0200

    finished the javascript babel es6 examples

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,8 @@
 var $ = require('jquery');
-var Person = require('./modules/Person');
+import Person from "./modules/Person";
 
 var john = new Person("John Doe", "blue");
 john.greet();
 
-var jane = new Person("Jane Smith", "green");
+var jane = new Person("Jane Smith", "orange");
 jane.greet();
--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -67,18 +67,61 @@
 /* 0 */
 /***/ (function(module, exports, __webpack_require__) {
 
-var $ = __webpack_require__(1);
-var Person = __webpack_require__(2);
+"use strict";
 
-var john = new Person("John Doe", "blue");
+
+var _Person = __webpack_require__(1);
+
+var _Person2 = _interopRequireDefault(_Person);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var $ = __webpack_require__(2);
+
+
+var john = new _Person2.default("John Doe", "blue");
 john.greet();
 
-var jane = new Person("Jane Smith", "green");
+var jane = new _Person2.default("Jane Smith", "orange");
 jane.greet();
-
 
 /***/ }),
 /* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Person = function () {
+  function Person(fullName, favColor) {
+    _classCallCheck(this, Person);
+
+    this.name = fullName;
+    this.favoriteColor = favColor;
+  }
+
+  _createClass(Person, [{
+    key: "greet",
+    value: function greet() {
+      console.log("Hi there, my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");
+    }
+  }]);
+
+  return Person;
+}();
+
+exports.default = Person;
+
+/***/ }),
+/* 2 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*!
@@ -10446,21 +10489,6 @@ if ( !noGlobal ) {
 
 return jQuery;
 } );
-
-
-/***/ }),
-/* 2 */
-/***/ (function(module, exports) {
-
-function Person (fullName, favColor) {
-  this.name = fullName;
-  this.favoriteColor = favColor;
-  this.greet = function () {
-    console.log("Hello, my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");
-  }
-}
-
-module.exports = Person;
 
 
 /***/ })
